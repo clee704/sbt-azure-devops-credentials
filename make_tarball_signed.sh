53dc4eeab3ff7e4dc3e1556b6c1ec8a5ebd40a76
Chungmin Lee <chungminlee@microsoft.com>
2024-06-13 06:57:35 +0000

    Clean up repo after creating tarball

--- make_tarball_signed.sh
+++ make_tarball_signed.sh
@@ -5,6 +5,7 @@ tarball_name="sbt-azure-devops-credentials_2.12_1.0-$version.tar.gz"
 GPG_TTY=$(tty) sbt 'set publishTo := Some(Resolver.mavenLocal)' publishSigned
 pushd "$HOME/.m2/repository"
 tar cvzf "$tarball_name" "dev/chungmin/sbt-azure-devops-credentials_2.12_1.0/$version"
+rm -rf "dev/chungmin/sbt-azure-devops-credentials_2.12_1.0/$version"
 popd
 mv "$HOME/.m2/repository/$tarball_name" .
 echo "$tarball_name"
